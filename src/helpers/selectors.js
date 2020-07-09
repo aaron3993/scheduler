6ac32046a308631b31b780ed060f6770513d3a28
aaron3993 <aaron3993@hotmail.com>
2020-07-09 19:18:24 -0400

    Added getInterviewersByDay and added validation to it as well as getAppointmentsForDay because I didn't do it properly last time

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,8 +3,10 @@ const getAppointmentsForDay = (state, day) => {
   for (const eachDay of state.days) {
     if (eachDay.name === day) {
       for (const appointment of eachDay.appointments) {
-        if (appointment === state.appointments[appointment].id) {
-          output.push(state.appointments[appointment])
+        if (state.appointments[appointment]) {
+          if (appointment === state.appointments[appointment].id) {
+            output.push(state.appointments[appointment])
+          }
         }
       }
     }
@@ -26,4 +28,65 @@ const getInterview = (state, interview) => {
   return output
 }
 
-export { getAppointmentsForDay, getInterview }
+const getInterviewersByDay = (state, day) => {
+  const output = []
+  for (const eachDay of state.days) {
+    if (eachDay.name === day) {
+      for (const interviewer of eachDay.interviewers) {
+        if (state.interviewers[interviewer]) {
+          if (interviewer === state.interviewers[interviewer].id) {
+            output.push(state.interviewers[interviewer])
+          }
+        }
+      }
+    }
+  }
+  return output
+}
+
+const state = {
+  days: [
+    {
+      id: 1,
+      name: "Monday",
+      appointments: [1, 2, 3],
+      interviewers: [1, 2]
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      appointments: [4, 5],
+      interviewers: [3, 4]
+    }
+  ],
+  appointments: {
+    "1": { id: 1, time: "12pm", interview: null },
+    "2": { id: 2, time: "1pm", interview: null },
+    "3": {
+      id: 3,
+      time: "2pm",
+      interview: { student: "Archie Cohen", interviewer: 2 }
+    },
+    "4": { id: 4, time: "3pm", interview: null },
+    "5": {
+      id: 5,
+      time: "4pm",
+      interview: { student: "Chad Takahashi", interviewer: 2 }
+    }
+  },
+  interviewers: {
+    "1": {  
+      id: 1,
+      "name": "Sylvia Palmer",
+      "avatar": "https://i.imgur.com/LpaY82x.png"
+    },
+    "2": {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    }
+  }
+};
+// console.log(getAppointmentsForDay(state, "Tuesday"))
+console.log(getInterviewersByDay(state, "Tuesday"))
+// export { getAppointmentsForDay, getInterview }
