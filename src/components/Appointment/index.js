b2507743d1732920b31d44a906d36f3f647f6fb2
aaron3993 <aaron3993@hotmail.com>
2020-07-09 15:44:26 -0400

    Completed the transition and back functions on useVisualMode.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,10 +3,18 @@ import React from 'react';
 import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
+import useVisualMode from "../../hooks/useVisualMode"
 import classnames from "classnames"
 import "components/Appointment/styles.scss"
 
 export default function Appointment (props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   const appointmentClass = classnames("appointment", {
     "appointment:last-of-type": props.id === "last"
   });
