8499150e67fcf005087f6eebe1cd0e444f2001e9
aaron3993 <aaron3993@hotmail.com>
2020-07-10 15:11:22 -0400

    Attempted to add saving status component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
 import Form from "./Form"
+import Status from "./Status"
 import useVisualMode from "../../hooks/useVisualMode"
 import classnames from "classnames"
 import "components/Appointment/styles.scss"
@@ -12,6 +13,7 @@ export default function Appointment (props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -27,6 +29,7 @@ export default function Appointment (props) {
       interviewer
     };
     console.log(interview, "save function")
+    transition(SAVING)
     props.bookInterview(props.id, interview).then(() => {
       transition(SHOW)
     })
@@ -50,7 +53,7 @@ export default function Appointment (props) {
         onSave={save}
         onCancel={back}/>
       )}
-      
+      {mode === SAVING && <Status />}
     </article>
   )
 }
