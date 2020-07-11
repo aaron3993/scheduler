dd79cfe686c6576888439be04caed7e02f007d05
aaron3993 <aaron3993@hotmail.com>
2020-07-11 13:53:45 -0400

    Brought back spots remaining functinality

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,37 +2,38 @@ import { useState, useEffect, useReducer } from "react";
 import axios from 'axios'
 
 export default function useApplicationData() {
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
+// const SET_DAY = "SET_DAY";
+// const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+// const SET_INTERVIEW = "SET_INTERVIEW";
 
-// const [state, setState] = useState({
-//   day: "Monday",
-//   days: [],
-//   appointments: {},
-//   interviewers: {},
-// });
+const [state, setState] = useState({
+  day: "Monday",
+  days: [],
+  appointments: {},
+  interviewers: {},
+});
 
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { /* insert logic */ }
-    case SET_APPLICATION_DATA:
-      return { /* insert logic */ }
-    case SET_INTERVIEW: {
-      return /* insert logic */
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-    }
-  }
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case SET_DAY:
+//       return { /* insert logic */ }
+//     case SET_APPLICATION_DATA:
+//       return { /* insert logic */ }
+//     case SET_INTERVIEW: {
+//       return /* insert logic */
+//     }
+//     default:
+//       throw new Error(
+//         `Tried to reduce with unsupported action type: ${action.type}`
+//       );
+//     }
+//   }
 
   // function scheuler() {
-    const [state, dispatch] = useReducer(reducer)
+    // const [state, dispatch] = useReducer(reducer)
   
-    const setDay = day => dispatch({ type: SET_DAY, day });
+    // const setDay = day => dispatch({ type: SET_DAY, day });
+    const setDay = day => setState({...state, day });
 
     useEffect(() => {
       const getDaysPromise = axios.get("/api/days")
@@ -44,13 +45,20 @@ function reducer(state, action) {
         getInterviewersPromise
       ])
       .then(all => {
-        dispatch(prev => ({
-          type: SET_APPLICATION_DATA,
+        setState(prev => ({
           days: all[0].data,
           appointments: all[1].data,
           interviewers: all[2].data
         }))
       })
+      // .then(all => {
+      //   dispatch(prev => ({
+      //     type: SET_APPLICATION_DATA,
+      //     days: all[0].data,
+      //     appointments: all[1].data,
+      //     interviewers: all[2].data
+      //   }))
+      // })
     }, [])
 
     function bookInterview(id, interview) {
@@ -111,11 +119,11 @@ function reducer(state, action) {
         });
       })
     }
+    return {
+      state,
+      setDay,
+      bookInterview,
+      cancelInterview
   }
-  return {
-    state,
-    setDay,
-    bookInterview,
-    cancelInterview
   }
 // }
