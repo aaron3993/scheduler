fa8ac2aa7732d3d178947c9be5903c54191320e9
aaron3993 <aaron3993@hotmail.com>
2020-07-11 20:32:12 -0400

    Wrapped spotsRemaining into a function and called it in the book and cancel interview functions

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,128 +2,92 @@ import { useState, useEffect, useReducer } from "react";
 import axios from 'axios'
 
 export default function useApplicationData() {
-// const SET_DAY = "SET_DAY";
-// const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-// const SET_INTERVIEW = "SET_INTERVIEW";
 
-const [state, setState] = useState({
-  day: "Monday",
-  days: [],
-  appointments: {},
-  interviewers: {},
-});
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
+  });
 
-// function reducer(state, action) {
-//   switch (action.type) {
-//     case SET_DAY:
-//       return { /* insert logic */ }
-//     case SET_APPLICATION_DATA:
-//       return { /* insert logic */ }
-//     case SET_INTERVIEW: {
-//       return /* insert logic */
-//     }
-//     default:
-//       throw new Error(
-//         `Tried to reduce with unsupported action type: ${action.type}`
-//       );
-//     }
-//   }
+  const setDay = day => setState({...state, day });
 
-  // function scheuler() {
-    // const [state, dispatch] = useReducer(reducer)
-  
-    // const setDay = day => dispatch({ type: SET_DAY, day });
-    const setDay = day => setState({...state, day });
+  useEffect(() => {
+    const getDaysPromise = axios.get("/api/days")
+    const getAppointmentsPromise = axios.get("/api/appointments")
+    const getInterviewersPromise = axios.get("/api/interviewers")
+    Promise.all([
+      getDaysPromise,
+      getAppointmentsPromise,
+      getInterviewersPromise
+    ])
+    .then(all => {
+      setState(prev => ({
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }))
+    })
+  }, [])
 
-    useEffect(() => {
-      const getDaysPromise = axios.get("/api/days")
-      const getAppointmentsPromise = axios.get("/api/appointments")
-      const getInterviewersPromise = axios.get("/api/interviewers")
-      Promise.all([
-        getDaysPromise,
-        getAppointmentsPromise,
-        getInterviewersPromise
-      ])
-      .then(all => {
-        setState(prev => ({
-          days: all[0].data,
-          appointments: all[1].data,
-          interviewers: all[2].data
-        }))
-      })
-      // .then(all => {
-      //   dispatch(prev => ({
-      //     type: SET_APPLICATION_DATA,
-      //     days: all[0].data,
-      //     appointments: all[1].data,
-      //     interviewers: all[2].data
-      //   }))
-      // })
-    }, [])
-
-    function bookInterview(id, interview) {
-      const appointment = {
-        ...state.appointments[id],
-        interview: { ...interview }
-      };
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-      let spotsRemaining = {}
-      // If an appointment already exists, that means we are editing it and will not decrease the spots remaining
-      if (!state.appointments[id].interview) {
-        for (const day of state.days) {
-          if (day.appointments.includes(id)) {
-            spotsRemaining = {
-              ...day,
-              spots: day.spots--
-            }
-          }
-        }
-      }
-      return axios.put(`/api/appointments/${id}`, appointment)
-      .then(response => {
-        setState({
-          ...state,
-          appointments,
-          spotsRemaining
-        });
-      })
-    }
-
-    function cancelInterview(id) {
-      const appointment = {
-        ...state.appointments[id],
-        interview: null
-      }
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      }
-      let spotsRemaining = {}
+  const setSpots = (id, interview) => {
+    let spotsRemaining = {}
       for (const day of state.days) {
         if (day.appointments.includes(id)) {
           spotsRemaining = {
             ...day,
-            spots: day.spots++
+            // Check to see if we are adding an interview, if so, then check if an interview already exists, if it does, we are editing it and will not change the spots
+            // remaining, if it doesn't a lready exist, it will subtract from spots remaing, and if we are not adding an interview, we are deleting and will add to spots remaining
+            spots: interview ? state.appointments[id].interview ? day.spots : day.spots-- : day.spots++
           }
         }
       }
-      return axios.delete(`/api/appointments/${id}`, appointment)
-      .then(response => {
-        setState({
-          ...state,
-          appointments,
-          spotsRemaining
-        });
-      })
+    return spotsRemaining
+  }
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    const spotsRemaining = setSpots(id, interview)
+    return axios.put(`/api/appointments/${id}`, appointment)
+    .then(response => {
+      setState({
+        ...state,
+        appointments,
+        spotsRemaining
+      });
+    })
+  }
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
     }
-    return {
-      state,
-      setDay,
-      bookInterview,
-      cancelInterview
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+    const spotsRemaining = setSpots(id)
+    return axios.delete(`/api/appointments/${id}`, appointment)
+    .then(response => {
+      setState({
+        ...state,
+        appointments,
+        spotsRemaining
+      });
+    })
   }
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
   }
-// }
+}
