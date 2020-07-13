66dd929545b9f2679b255a395116e636c0f125ad
aaron3993 <aaron3993@hotmail.com>
2020-07-13 12:44:58 -0400

    Installed proptypes and imported it to interviewerList.js

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
+import PropTypes from 'prop-types';
 import "components/InterviewerList.scss";
 
 export default function InterviewerList (props) {
