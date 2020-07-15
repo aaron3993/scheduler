7b71cdce5c012f7e9fafe689ecd354c249b7a464
aaron3993 <aaron3993@hotmail.com>
2020-07-14 20:37:41 -0400

    Refactored reducer into a separate folder and wrote a test for it, and excluded index.js from jest tests

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,36 +1,12 @@
 import { useEffect, useReducer } from "react";
-import { setSpots } from "../helpers/selectors"
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "reducers/application";
 import axios from 'axios'
 
 export default function useApplicationData() {
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case SET_DAY:
-        return {...state, day: action.day}
-      case SET_APPLICATION_DATA:
-        return {...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers}
-      case SET_INTERVIEW: {
-        const appointment = {
-        ...state.appointments[action.id],
-        interview: action.interview
-        };
-        const appointments = {
-          ...state.appointments,
-          [action.id]: appointment
-        };
-        const days = setSpots(state, appointments)
-        return {...state, appointments, days}
-        }
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-      );
-    }
-  }
 
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
---
+++ src/reducers/application.js
@@ -0,0 +1,32 @@
+import { setSpots } from "../helpers/selectors"
+
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+  export default function reducer(state, action) {
+    switch (action.type) {
+      case SET_DAY:
+        return {...state, day: action.day}
+      case SET_APPLICATION_DATA:
+        return {...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers}
+      case SET_INTERVIEW: {
+        const appointment = {
+        ...state.appointments[action.id],
+        interview: action.interview
+        };
+        const appointments = {
+          ...state.appointments,
+          [action.id]: appointment
+        };
+        const days = setSpots(state, appointments)
+        return {...state, appointments, days}
+        }
+      default:
+        throw new Error(
+          `Tried to reduce with unsupported action type: ${action.type}`
+      );
+    }
+  }
+
+  export { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW }
