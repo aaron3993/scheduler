823fcac55487e4480ba1a9e5c1a4efe36e53ac32
aaron3993 <aaron3993@hotmail.com>
2020-07-16 21:05:20 -0400

    Added netlify.toml and configured env variable in index.js

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -33,7 +33,7 @@ export default function useApplicationData() {
     Promise.all([
       axios.get("/api/days"),
       axios.get("/api/appointments"),
-      axios.get("/api/interviewers"),
+      axios.get("/api/interviewers")
     ])
     .then(([days, appointments, interviewers]) => {
       dispatch({
